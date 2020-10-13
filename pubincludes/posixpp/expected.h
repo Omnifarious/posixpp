b52fa436647086b87d7efd7c6af3422243b4069d
Eric Hopper <hopper@omnifarious.org>
2020-10-13 09:22:09 -0700

    Add noexcept as appropriate. Use constraints. Create version of... expected<T> for things that _only_ have an error return for consistency.

--- pubincludes/posixpp/expected.h
+++ pubincludes/posixpp/expected.h
@@ -4,6 +4,7 @@
 #include <utility>
 #include <stdexcept>
 #include <variant>
+#include <concepts>
 
 namespace posixpp {
 
@@ -21,20 +22,23 @@ class expected_base {
 };
 }
 
-template <class T>
+template <typename T>
 class expected : private priv_::expected_base {
  public:
    // Just a type to serve as a tag to indicate error value.
    using priv_::expected_base::err_tag;
 
-   explicit expected(T const &val) : val_{val}
+   explicit expected(T const &val) requires ::std::copyable<T> : val_{val}
    {}
-   explicit expected(T &&val) : val_{::std::move(val)}
+   explicit expected(T &&val) requires ::std::movable<T>
+           : val_{::std::move(val)}
    {}
-   explicit expected(err_tag const &, int ec) : val_{err_type{ec}}
+   explicit expected(err_tag const &, int ec)
+   requires ::std::movable<T> || ::std::copyable<T>
+           : val_{err_type{ec}}
    {}
 
-   [[nodiscard]] T &&result() {
+   [[nodiscard]] T &&result() requires ::std::movable<T> {
       if (auto result = ::std::get_if<T>(&val_)) {
          return ::std::move(*result);
       } else {
@@ -43,7 +47,7 @@ class expected : private priv_::expected_base {
       }
    }
 
-   [[nodiscard]] T const &result() const {
+   [[nodiscard]] T const &result() const  requires ::std::copyable<T> {
       if (auto result = ::std::get_if<T>(&val_)) {
          return *result;
       } else {
@@ -59,7 +63,7 @@ class expected : private priv_::expected_base {
       }
    }
 
-   [[nodiscard]] bool has_error() const {
+   [[nodiscard]] bool has_error() const noexcept {
       return ::std::holds_alternative<err_type>(val_);
    }
 
@@ -73,6 +77,41 @@ class expected : private priv_::expected_base {
 
  private:
    ::std::variant<T, err_type> val_;
+};
+
+template <>
+class expected<void> : private priv_::expected_base {
+ public:
+   expected() : err_(err_type{0}) {}
+   explicit expected(err_tag const &, int ec) noexcept
+           : err_{err_type{ec}}
+   {}
+
+   void result() const {
+      throw_if_error();
+   }
+
+   void throw_if_error() const {
+      if (err_ != 0) {
+         auto const &cat = ::std::system_category();
+         throw ::std::system_error(err_, cat);
+      }
+   }
+
+   [[nodiscard]] bool has_error() const noexcept {
+      return err_ != 0;
+   }
+
+   [[nodiscard]] int error() const noexcept {
+      return err_;
+   }
+
+   [[nodiscard]] ::std::error_condition error_condition() const noexcept {
+      return ::std::system_category().default_error_condition(error());
+   }
+
+ private:
+   err_type err_;
 };
 
 }
