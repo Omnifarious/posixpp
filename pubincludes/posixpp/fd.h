43d58932f953d61bd42bd682cf1883915dcfde6e
Eric Hopper <hopper@omnifarious.org>
2020-10-13 12:55:46 -0700

    Add a lot more 'noexcept's. Add some documenting comments.

--- pubincludes/posixpp/fd.h
+++ pubincludes/posixpp/fd.h
@@ -10,49 +10,54 @@
 
 namespace posixpp {
 
+//! A file descriptor and the associated functions
 class fd {
  public:
    // Usually, you shouldn't use this.
-   explicit fd(int fdval) : fd_(fdval) {}
-   ~fd() {
+   explicit fd(int fdval) noexcept : fd_(fdval) {}
+   ~fd() noexcept {
       using ::syscalls::linux::close;
       if (fd_ >= 0) {
-         close(fd_);
+         close(fd_); // Ignore any error return.
       }
    }
-   fd(fd &&other) : fd_(other.fd_) { other.fd_ = -1; }
-   fd &operator =(fd &&other) {
+   fd(fd &&other) noexcept : fd_(other.fd_) { other.fd_ = -1; }
+   fd &operator =(fd &&other) noexcept {
       using ::syscalls::linux::close;
       if (fd_ >= 0) {
-         close(fd_);
+         close(fd_); // Ignore any error return.
       }
       fd_ = other.fd_;
       other.fd_ = -1;
       return *this;
    }
 
-   int as_fd() const { return fd_; }
+   int as_fd() const noexcept { return fd_; }
 
-   static expected<fd> open(char const *pathname, int flags) {
+   static expected<fd> open(char const *pathname, int flags) noexcept {
       using ::syscalls::linux::open;
       using posixpp::error_cascade;
       return error_cascade(open(pathname, flags, 0),
                            [](auto r) { return fd{static_cast<int>(r)}; });
    }
-   static expected<fd> open(char const *pathname, int flags, ::mode_t mode) {
+   static expected<fd> open(
+           char const *pathname,
+           int flags,
+           ::mode_t mode) noexcept
+   {
       using ::syscalls::linux::open;
       using posixpp::error_cascade;
       return error_cascade(open(pathname, flags, mode),
                            [](auto r) { return fd{static_cast<int>(r)}; });
    }
 
-   expected<::std::size_t> write(char const *buf, ::std::size_t size) {
+   expected<::std::size_t> write(char const *buf, ::std::size_t size) noexcept {
       using posixpp::error_cascade;
       return error_cascade(::syscalls::linux::write(fd_, buf, size),
                            [](auto r) { return static_cast<::std::size_t>(r);});
    }
 
-   expected<::std::size_t> read(char *buf, ::std::size_t size) {
+   expected<::std::size_t> read(char *buf, ::std::size_t size) noexcept {
       using posixpp::error_cascade;
       return error_cascade(::syscalls::linux::read(fd_, buf, size),
                            [](auto r) { return static_cast<::std::size_t>(r);});
