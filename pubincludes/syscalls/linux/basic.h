6f16e8cc12721de9f98249eace54665875c8d52c
Eric Hopper <hopper@omnifarious.org>
2022-01-10 10:41:44 -0800

    Silence "return from norreturn"  warnings.

--- pubincludes/syscalls/linux/basic.h
+++ pubincludes/syscalls/linux/basic.h
@@ -10,10 +10,12 @@ namespace linux {
 
 inline void exit [[noreturn]](int status) noexcept {
    syscall_expected(call_id::exit, status);
+   __builtin_unreachable();
 }
 
 inline void exit_group [[noreturn]](int status) noexcept {
    syscall_expected(call_id::exit_group, status);
+   __builtin_unreachable();
 }
 
 } // namespace linux
